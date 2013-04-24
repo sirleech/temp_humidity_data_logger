da43e1929cae1cabaa3991c8c7a45286554d6b7a
chris <chris@chris-medion-ubuntu.(none)>
2013-04-25 08:11:22 +1000

    output a human readable one using python datetime

--- csv_utils/localise.py
+++ csv_utils/localise.py
@@ -1,21 +1,35 @@
 import csv
+import datetime
 
 ifile  = open('../sample_data/DATA.CSV', 'rb')
 ofile  = open('../sample_data/localised.CSV', 'wb')
+ofile_readable  = open('../sample_data/localised_readable.CSV', 'wb')
 reader = csv.reader(ifile, delimiter=',', quotechar='"')
 writer = csv.writer(ofile, delimiter=',', quotechar='"')
+writer_readable = csv.writer(ofile_readable, delimiter=',', quotechar='"')
 
-header = ["Time UTC+10","Temperature (c)","RH (%)"]
+header = ["Time (local)","Temperature (c)","RH (%)"]
+
+# write first row of CSV as headers
 writer.writerow(header)
+writer_readable.writerow(header)
 
 reader.next()
 for row in reader:
-	unix_time = row[0]
+	
 	# adjust time to local timezone
 	# utc + 10 AEST
+
+	unix_time = row[0]
 	offset = 10 * 60 * 60
-	row[0] = int(unix_time) + offset
+	unix_time_adjusted = int(unix_time) + offset
+	row[0] = unix_time_adjusted
+
 	writer.writerow(row)
+	readable_time = datetime.datetime.fromtimestamp(int(unix_time)).strftime('%Y-%m-%d %H:%M:%S')
+	row[0] = readable_time
+	writer_readable.writerow(row)
 
 ifile.close()
 ofile.close()
+ofile_readable.close()
